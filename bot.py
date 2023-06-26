5960bcd4f48b4b246838fe783286014562dd71c5
qalmurri <86642474+qalmurri@users.noreply.github.com>
2023-06-26 17:33:27 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -52,45 +52,45 @@ async def update_leaderboard():
             # Melakukan perbaruan leaderboard_chat di text channel
             data_chat = load_data_chat()
             chats = data_chat['chats']
-            levels = data_chat['chat_levels']
-            combined_data = [(user_id, chats[user_id], levels[user_id]) for user_id in chats]
+            chat_levels = data_chat['chat_levels']
+            combined_data_chat = [(user_id, chats[user_id], chat_levels[user_id]) for user_id in chats]
             #print(combined_data)
-            sorted_chats = sorted(combined_data, key=lambda x: x[1], reverse=True)
+            sorted_data_chat = sorted(combined_data_chat, key=lambda x: x[1], reverse=True)
             output = f"> :incoming_envelope: **LEADERBOARD CHAT** :incoming_envelope:\n> *Diupdate* {date}\n" 
-            for i, (user_id, message_count, level) in enumerate(sorted_chats, start=1):
+            for i, (user_id, message_count, level) in enumerate(sorted_data_chat, start=1):
                 try:
                     user = await bot.fetch_user(int(user_id))
                     output += f"{i}. `{user.name}` `({level})` `{message_count} pesan`\n"
                 except discord.NotFound:
                     output += f"{i}. User tidak ditemukan: {message_count} pesan\n"
-            additional_text = f"> :incoming_envelope: **LEADERBOARD CHAT** :incoming_envelope:\n> *Diupdate* {date}\n" 
-            output += additional_text
+            #additional_text = f"> :incoming_envelope: **LEADERBOARD CHAT** :incoming_envelope:\n> *Diupdate* {date}\n" 
+            #output += additional_text
             await message_chat.edit(content=output)
 
             # Melakukan perbaruan leaderboard_voice di text channel
             data_voice = load_data_voice()
-            total_data = data_voice['total']
+            total = data_voice['total']
             level_data = data_voice['voice_levels']
             #combined_data_voice = [(user_id, total_data[user_id], level_data[user_id]) for user_id in total_data]
-            combined_data_voice = [(user_id, total_data[user_id]['total_time'], level_data[user_id]) for user_id in total_data]
+            combined_data_voice = [(user_id, total[user_id]['total_time'], level_data[user_id]) for user_id in total]
             sorted_chats_voice = sorted(combined_data_voice, key=lambda x: x[1], reverse=True)
             #sorted_voice = sorted(total_data.items(), key=lambda x: x[1], reverse=True)
             output = f"> :sound: **LEADERBOARD VOICE** :sound:\n> *Diupdate* {date}\n" 
             #for i, (user_id, stats, level_voice) in enumerate(sorted_chats_voice, start=1):
             #    total_time = stats['total_time']
             for i, (user_id, stats, level_voice) in enumerate(sorted_chats_voice, start=1):
-                total_time = stats
+                convert = stats
                 # conver time
-                struct_time = time.gmtime(total_time)
-                formatted_time = time.strftime("`%m/%d %H:%M:%S`", struct_time)
+                convert2 = time.gmtime(convert)
+                total_time = time.strftime("`%m/%d %H:%M:%S`", convert2)
                 #print(f"{i}. User ID: {user_id}, Total Time: {total_time}")
                 try:
                     user = await bot.fetch_user(int(user_id))
-                    output += f"{i}. `{user.name}` `({level_voice})` {formatted_time}\n"
+                    output += f"{i}. `{user.name}` `({level_voice})` {total_time}\n"
                 except discord.NotFound:
-                    output += f"{i}. User tidak ditemukan: {formatted_time} detik\n"
-            additional_text2 = f"> :sound: **LEADERBOARD VOICE** :sound:\n> *Diupdate* {date}\n" 
-            output += additional_text2
+                    output += f"{i}. User tidak ditemukan: {total_time} detik\n"
+            #additional_text2 = f"> :sound: **LEADERBOARD VOICE** :sound:\n> *Diupdate* {date}\n" 
+            #output += additional_text2
             await message_voice.edit(content=output)
 
             # Menunggu interval waktu sebelum melakukan perbaruan selanjutnya/interval waktu dalam detik
@@ -123,12 +123,12 @@ async def update_chat_levels(data_chat, user_id, message_count):
     chat_levels = data_chat['chat_levels']
     if str(user_id) in chat_levels:
         current_level = chat_levels[str(user_id)]
-        if message_count >= current_level * 100:
+        if message_count >= current_level * 1:
             chat_levels[str(user_id)] += 1
             #kirim pesan ke channel jika level up
-            levelup = chat_levels[str(user_id)]
+            chat_levelup = chat_levels[str(user_id)]
             channel_chats = bot.get_channel(mention_levelup)
-            texts = [f"Selamat <@{str(user_id)}>, kategori chat mu naik ke level {levelup}.", f"Anjay! Chat <@{str(user_id)}> sekarang di level {levelup}.", f"<@{str(user_id)}> yang jarang tidur, Chat aja bisa level {levelup}.", f"Keren <@{str(user_id)}> udah level {levelup}, Chatingan sama siapa aja?", f"Lagi gabut ya <@{str(user_id)}>? sampe-sampe Chat udah level {levelup}.", f"Maniak Chat ya <@{str(user_id)}>, gak kerasa Chat udah level {levelup}.", f"<@{str(user_id)}> rajin amat nge-Chat, padahal sudah level {levelup}.", f"Chat tanpa <@{str(user_id)}> sepi, Terima Kasih sudah level {levelup}.", f"<@{str(user_id)}> Chat terbaik! Terima kasih sudah level {levelup} :crown:", f"<@{str(user_id)}> :two_hearts: Chatmu sekarang sudah level {levelup}."]
+            texts = [f"Selamat <@{str(user_id)}>, kategori chat mu naik ke level {chat_levelup}.", f"Anjay! Chat <@{str(user_id)}> sekarang di level {chat_levelup}.", f"<@{str(user_id)}> yang jarang tidur, Chat aja bisa level {chat_levelup}.", f"Keren <@{str(user_id)}> udah level {chat_levelup}, Chatingan sama siapa aja?", f"Lagi gabut ya <@{str(user_id)}>? sampe-sampe Chat udah level {chat_levelup}.", f"Maniak Chat ya <@{str(user_id)}>, gak kerasa Chat udah level {chat_levelup}.", f"<@{str(user_id)}> rajin amat nge-Chat, padahal sudah level {chat_levelup}.", f"Chat tanpa <@{str(user_id)}> sepi, Terima Kasih sudah level {chat_levelup}.", f"<@{str(user_id)}> Chat terbaik! Terima kasih sudah level {chat_levelup} :crown:", f"<@{str(user_id)}> :two_hearts: Chatmu sekarang sudah level {chat_levelup}."]
             random_text = random.choice(texts)
             await channel_chats.send(random_text)
     else:
@@ -136,20 +136,20 @@ async def update_chat_levels(data_chat, user_id, message_count):
 
 # Memperbarui level user
 async def update_voice_levels(data_voice, user_id):
-    level = data_voice['voice_levels']
-    if str(user_id) in level:
-        current_level = level[str(user_id)]
-        total = data_voice['total'][user_id]['total_time']
-        lvl = total // 1600
+    voice_levels = data_voice['voice_levels']
+    if str(user_id) in voice_levels:
+        current_level = voice_levels[str(user_id)]
+        total_time = data_voice['total'][user_id]['total_time']
+        level = total_time // 16
         #1600 = 00:26:40
-        if lvl >= current_level:
-            level[str(user_id)] += 1
-            levelup2 = level[str(user_id)]
-            channel_chats = bot.get_channel(mention_levelup)
-            #{str(user_id)} {levelup2}
-            textvoice = [f"Gwendeng, wes bosen koen <@{str(user_id)}>? Voicemu wes level {levelup2}.", f"Selamat <@{str(user_id)}>, kategori voice mu naik ke level {levelup2}.", f"Sumpek a <@{str(user_id)}>? Voicemu wes level {levelup2}.", f"Emang kalo gak ada <@{str(user_id)}> voice jadi sepi, voicemu level {levelup2} .", f"<@{str(user_id)}> kenapa udahan? Padahal lagi seru-serunya, voicemu udah level {levelup2}.", f"<@{str(user_id)}> Voice terbaik! Terima kasih sudah level {levelup2}."]
-            random_text_voice = random.choice(textvoice)
-            await channel_chats.send(random_text_voice)
+        if level >= current_level:
+            voice_levels[str(user_id)] += 1
+            voice_levelup = voice_levels[str(user_id)]
+            channel_voice = bot.get_channel(mention_levelup)
+            #{str(user_id)} {voice_levelup}
+            randomvoice_levelup = [f"Gwendeng, wes bosen koen <@{str(user_id)}>? Voicemu wes level {voice_levelup}.", f"Selamat <@{str(user_id)}>, kategori voice mu naik ke level {voice_levelup}.", f"Sumpek a <@{str(user_id)}>? Voicemu wes level {voice_levelup}.", f"Emang kalo gak ada <@{str(user_id)}> voice jadi sepi, voicemu level {voice_levelup} .", f"<@{str(user_id)}> kenapa udahan? Padahal lagi seru-serunya, voicemu udah level {voice_levelup}.", f"<@{str(user_id)}> Voice terbaik! Terima kasih sudah level {voice_levelup}."]
+            textvoice_levelup = random.choice(randomvoice_levelup)
+            await channel_voice.send(textvoice_levelup)
     else:
         level[str(user_id)] = 1
     save_data_voice(data_voice)
